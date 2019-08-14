856686c06cdb763b97eb7114bdc524dd18970be5
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2019-08-15 01:03:11 +0200

    Add 3d unet training

--- utils/threshold_nuclei_predictions.py
+++ utils/threshold_nuclei_predictions.py
@@ -1,15 +1,24 @@
 import h5py
 import numpy as np
+from skimage import measure
 
 element_size_um = np.array([1., 0.17297205, 0.17297205])
-in_path = '/home/adrian/workspace/ilastik-datasets/Vladyslav/vlad_nuclei_predictions.h5'
-out_path = '/home/adrian/workspace/ilastik-datasets/Vladyslav/vlad_nuclei_segm.h5'
-with h5py.File(in_path, 'r+') as f:
+in_path = '/home/adrian/workspace/ilastik-datasets/Ovules/Nuclei/28_predictions.h5'
+out_path = '/home/adrian/workspace/ilastik-datasets/Ovules/Nuclei/28_segm.h5'
+
+with h5py.File(in_path, 'r') as f:
     preds = f['predictions'][0]
 
-    thresholds = [0.5, 0.8, 0.9]
-    for t in thresholds:
-        mask = preds > t
-        mask = mask.astype(np.uint8)
-        ds = f.create_dataset('nuclei_segm_' + str(t), data=mask, compression='gzip')
-        ds.attrs['element_size_um'] = element_size_um
+    with h5py.File(out_path, 'w') as g:
+        thresholds = [0.5, 0.65, 0.8]
+        for t in thresholds:
+            print(f'Processing threshold {t}...')
+            mask = preds > t
+            mask = mask.astype(np.uint8)
+            ds = g.create_dataset('nuclei_mask_' + str(t), data=mask, compression='gzip')
+            ds.attrs['element_size_um'] = element_size_um
+
+            instance_segm = measure.label(mask, connectivity=1)
+            instance_segm = instance_segm.astype('uint16')
+            ds = g.create_dataset('nuclei_segm_' + str(t), data=instance_segm, compression='gzip')
+            ds.attrs['element_size_um'] = element_size_um
