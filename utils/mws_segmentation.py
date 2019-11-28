6f3a46842960cca2addde2746a2aa36ec904430d
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2019-11-28 17:12:53 +0100

    Update mws prediction

--- utils/mws_segmentation.py
+++ utils/mws_segmentation.py
@@ -1,8 +1,37 @@
 # import the segmentation functionality from elf
 import h5py
 import numpy as np
-import elf.segmentation.mutex_watershed as mws
+from affogato.segmentation import compute_mws_segmentation
 
+
+
+def mutex_watershed(affs, offsets, strides,
+                    randomize_strides=False, mask=None,
+                    noise_level=0):
+    """ Compute mutex watershed segmentation.
+
+    Introduced in "The Mutex Watershed and its Objective: Efficient, Parameter-Free Image Partitioning":
+    https://arxiv.org/pdf/1904.12654.pdf
+
+    Arguments:
+        affs [np.ndarray] - input affinity map
+        offsets [list[list[int]]] - pixel offsets corresponding to affinity channels
+        strides [list[int]] - strides used to sub-sample long range edges
+        randomize_strides [bool] - randomize the strides? (default: False)
+        mask [np.ndarray] - mask to exclude from segmentation (default: None)
+        noise_level [float] - sigma of noise added to affinities (default: 0)
+    """
+    ndim = len(offsets[0])
+    if noise_level > 0:
+        affs += noise_level * np.random.rand(*affs.shape)
+    affs[:ndim] *= -1
+    affs[:ndim] += 1
+    seg = compute_mws_segmentation(affs, offsets,
+                                   number_of_attractive_channels=ndim,
+                                   strides=strides, mask=mask,
+                                   randomize_strides=randomize_strides)
+    #relabelConsecutive(seg, out=seg, start_label=1, keep_zeros=mask is not None)
+    return seg
 
 in_file = '/g/kreshuk/wolny/Datasets/Vladyslav/GT/GT_Ab1_test1_predictions_affinities.h5'
 out_file = '/g/kreshuk/wolny/Datasets/Vladyslav/GT/GT_Ab1_test1_predictions_mws.h5'
@@ -22,7 +51,7 @@ strides = [1, 6, 6]
 randomize_strides = True
 
 print('Executing MWS...')
-segmentation = mws.mutex_watershed(affs, offsets, strides, randomize_strides=randomize_strides)
+segmentation = mutex_watershed(affs, offsets, strides, randomize_strides=randomize_strides)
 
 segmentation = segmentation.astype('uint16')
 
